7c791782fd8ff3ad1be0c0f091035231c96a2374
tvkeprof <wefousoul@gmail.com>
2024-12-02 16:40:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+
+// data types //
+// 1. string => text type => "text"
+// 2. number => number type => 1 6 1000 
+// 3. boolean => true or false type => false/true 
+// 4. object => {key:value}
+// 5. array => [index,index ]
+// 6. Null or Undefined => no value
